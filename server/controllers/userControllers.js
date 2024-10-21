455a84b69910c59c3f768d942a75fa3206a30ec6
bhanu <bhanu2003@gmail.com>
2024-10-21 15:20:44 +0530

    added clearing cart controller

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -134,6 +134,25 @@ exports.getUserCart = async (req, res) => {
   }
 };
 
+exports.clearCart = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.cart = [];
+    await user.save();
+
+    return res.status(200).json({ message: "Cart cleared successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
 exports.addToBookmarks = async (req, res) => {
   const { userId, restaurantId } = req.body;
 
